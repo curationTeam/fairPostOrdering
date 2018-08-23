e69fc29867c113ff3b5f1ed00d00b119d9b257d6
Orfeas Stefanos Thyfronitis Litos <o.thyfronitis@ed.ac.uk>
2018-08-23 15:49:56 +0100

    Lint, change var names, other minor

--- scripts/player.py
+++ scripts/player.py
@@ -5,72 +5,65 @@ class Player:
     """
     Player class which determines the properties of a player.
     A player is determined by:
-        id(int) -  id of the player
-        quality(int,int) - Mean and st deviation of the quality of the posts created by the player
-        strategy(Strategy) -  Strategy followed by the player
-        vp(int)  -  Current Voting Power that the player has.
-        attention(int) - Number of post that the player can view in a round.
+        id(int) - id of the player
+        quality(int, int) - Mean and st deviation of the quality of the posts created by the player
+        strategy(Strategy) - Strategy followed by the player
+        vp(int) - Current Voting Power that the player has.
+        attention(int) - Number of posts that the player can view in a round.
     """
-    def __init__(self, id, quality_mean, quality_st, type, sp, attention):
 
+    def __init__(self, id, quality_mean, quality_sd, type, sp, attention):
         self.id = id
-        self.quality = [quality_mean,quality_st]
-        self.strategy = Strategy(type,id)
+        self.quality = [quality_mean, quality_sd]
+        self.strategy = Strategy(type, id)
         self.sp = sp
         self.vp = 1
         self.attention = attention
-        
-    def set_strategy(self, strategy):
 
+    def set_strategy(self, strategy):
         """
         Set core strategy of the player (e.g. honest or greedy).
         """
         self.strategy = strategy
 
     def set_sp(self, sp):
-        
         """
         Set Steem Power of the player.
         """
         self.sp = sp
 
     def set_vp(self, vp):
-
         """
         Set Voting Power of the player.
         """
         self.vp = vp
 
-    def set_quality(self,quality_mean, quality_st):
-
+    def set_quality(self, quality_mean, quality_sd):
         """
         Set quality of "writing posts" of the player.
         """
+        self.quality = [quality_mean, quality_sd]
 
-        self.quality = [quality_mean,quality_st]
 
-    def create_post(self,quality,players):
 
+    def create_post(self, quality, players):
         """
         Player creates a Post given its quality parameters.
         Return:
         post(Post) - Post created by the player
         """
-
-        post = Post(self.id,self.quality, players)
+        post = Post(self.id, self.quality, players)
         return post
 
-    def vote(self,posts):
-
+    def vote(self, posts):
         """
         Player votes for a post from a list of posts only if her Voting Power is greater than the Minimum
         Voting Power defined by her Strategy.
-        Return: 
-        post(Post) - post to be voted by the player (False if the player do not vote for any post) 
+        Return:
+        post(Post) - post to be voted by the player (False if the player does not vote for any post)
         """
-
         #Only vote if voting power greater or equal than minimum voting power of the strategy
         if self.vp >= self.strategy.min_vp:
-            return self.strategy.vote(posts,self.attention)
+            return self.strategy.vote(posts, self.attention)
         else:
             return False
--- scripts/simulation.py
+++ scripts/simulation.py
@@ -1,4 +1,3 @@
-
 from scipy import stats
 import random
 
@@ -6,95 +5,88 @@ from strategy import Strategy
 from player import Player
 from post import Post
 
-#honest and greedy content creators. users only vote and do not create content (and are honest)
-profile = ["honest","greedy","user"]
+# honest and greedy content creators. users only vote and do not create content (and are honest)
+profile = ["honest", "greedy", "user"]
 
-#noProfiles should be a list of integers [3,4,5] denoting the number of players per strategy profile
+# noProfiles should be a list of integers [3, 4, 5] denoting the number of players per strategy profile
 
 '''
 voting_power usage per vote           -->    VP' = VP - (a*VP*w + b)
-voting_power regeneration per round   -->    VP' = min (VP + c, 1)
-attention span --> d
+voting_power regeneration per round   -->    VP' = min (VP + regen, 1)
+attention span --> att_span
 '''
 
-def simulation(seed,rounds,nStrategies,a,b,c,d): #poner nStrategies en tuple
+def simulation(seed, rounds, noProfiles, a, b, regen, att_span): # TODO: poner nStrategies en tuple
+    assert type(noProfiles) == list
 
-    assert type(nStrategies) == list
+    players, posts = init_setup(seed, noProfiles, att_span)
 
-    players,posts = init_setup(seed,nStrategies,d)
-
-    for _round in range(rounds):
-        random.shuffle(players) #randomize voting order in each round
+    for _round in range(0, rounds):
+        random.shuffle(players) # randomize voting order in each round
 
         for player in players:
-
-            player = regenerate_vp(player, c)
+            player.regenerate_vp(regen) # TODO: We have to define c in terms of the rounds
             post = player.vote(posts)
             #print(post)
-            if post != False: #Check if the player actually vote for something
-                player,posts = execute_vote(player,post,posts,a,b)
+            if post != False: #Check if the player actually voted for something
+                player, posts = execute_vote(player, post, posts, a, b)
 
     print_result(posts) #ojo que los ordena por calidad...MIRAR ESTO---------------T0D0
 
-    return posts,players
+    return posts, players
 
-#Reseed and initialize players and posts
-def init_setup(seed, noProfiles,d):
-        
-    random.seed(seed) #reseed the prng
-    players = init_players(noProfiles,d)
+# Reseed and initialize players and posts
+def init_setup(seed, noProfiles, att_span):
+    random.seed(seed) # reseed the prng
+    players = init_players(noProfiles, att_span)
     posts = init_posts(players)
 
-    return players,posts
-    
-#Initialize players
-def init_players(noProfiles,d):
-    
+    return players, posts
+
+# Initialize players
+def init_players(noProfiles, att_span):
     players = []
     index = 0
     profile_index = 0
 
-
     for noProfile in noProfiles:
-        #Create noProfile players of profile_index and profile = ["honest", "greedy", "user"]
+        # Create noProfile players of profile_index and profile = ["honest", "greedy", "user"]
         for _i in range(0, noProfile):
-            #Add a player (id,quality_range,strategy_profile,sp)
-            players.append(Player(index,random.uniform(0,1),0.1,profile[profile_index],1,d))
+            # Add a player (id, quality_range, strategy_profile, sp)
+            mean = random.uniform(0, 1)
+            std = 0.1
+            players.append(Player(index, mean, std, profile[profile_index], 1, att_span))
             index += 1
 
         profile_index += 1
 
     return players
 
-#Initialize posts
+# Initialize posts
 def init_posts(players):
     posts = []
-    players_ids = list(range(len(players)))
+    players_ids = list(range(0, len(players)))
     for player in players:
-
-        #users do not create posts
+        # users do not create posts
         if str(player.strategy) != "user":
-
             assert (str(player.strategy) == "honest" or str(player.strategy) == "honest")
 
             post = player.create_post(player.quality, players_ids)
             posts.append(post)
-        
-    random.shuffle(posts) #randomize initial order of post ranking
+
+    random.shuffle(posts) # randomize initial order of post ranking
     initial_order_posts = display_list(posts)
     print('Initial order of posts:', initial_order_posts)
 
     return posts
 
-def execute_vote(player,post,posts,a,b):
-
-    #Calculate vote as the product of player sp, player current vp and likability(weight)
+def execute_vote(player, post, posts, a, b):
+    # Calculate vote as the product of player sp, player current vp and likability(weight)
     weight = post.likability[player.id]
     post.votes_received += player.sp * player.vp * weight
     post.voters.append(player.id)
     player = recalculate_vp(player,a,b) #Decrease voting power after vote
-    posts.sort(key=lambda x: x.votes_received, reverse = True) #order post ranking by votes received
-    return player,posts
+    posts.sort(key=lambda x: x.votes_received, reverse = True) # Order post ranking by votes received
 
 def recalculate_vp(player,a,b):
     
@@ -119,6 +111,7 @@ def get_number_players(noProfiles):
         ctr += profile
 
     return ctr
+    return player, posts
 
 # Return a list with the author_id of the posts
 def display_list(posts):
@@ -136,14 +129,14 @@ def get_attacker_id(players):
     return attacker_id
 
 # Calculate the net position difference of the attacker against its expected position ---------WIP
-def net_position(players,posts):
+def net_position(players, posts):
     ctr = 0
     attacker_id = get_attacker_id(players)
     for p in posts:
         if p.author_id == attacker_id:
             expected_quality_index = len(posts) - 1 - p.quality
             return ctr - expected_quality_index
-        ctr += 1 
+        ctr += 1
 
 # Sort lists of posts by quality
 def sort_by_quality(posts):
@@ -154,19 +147,16 @@ def sort_by_quality(posts):
 
 # Print results of execution
 def print_result(posts):
-
     order_posts = display_list(posts)
     quality_sorted = sort_by_quality(posts)
-    print ('Final ranking of posts:',order_posts)
-    print ('Quality sorted:',quality_sorted)
-    print ('Spearman:',stats.spearmanr(quality_sorted, order_posts)[0])
-    print ('KendallTau:',stats.kendalltau(quality_sorted, order_posts)[0])
+    print('Final ranking of posts:', order_posts)
+    print('Quality sorted:', quality_sorted)
+    print('Spearman:', stats.spearmanr(quality_sorted, order_posts)[0])
+    print('KendallTau:', stats.kendalltau(quality_sorted, order_posts)[0])
 
 def main():
-  
-  seed = random.randint(0,1000)
-  simulation(seed,10,[15,0,10],0,0,1,5)
+  seed = random.randint(0, 1000)
+  simulation(seed, 10, [15, 0, 10], 0, 0, 1, 5)
 
-  
 if __name__== "__main__":
   main()
