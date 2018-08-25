e6c9d4623795ef86798dba47aea372d9b8357afd
monty <amonteolivam@gmail.com>
2018-08-25 12:19:26 +0200

    calculate t-similar with updated posts

--- scripts/simulation/main.py
+++ scripts/simulation/main.py
@@ -7,7 +7,7 @@ def main():
   # Simulation(rounds, noProfiles, a, b, regen_time, att_span)
   sim = Simulation(201600, (15, 0, 10), 1/200, 1, 6.94*10-6, 10)
   players, posts = sim.init_setup(seed)
-  sim.execute(players, posts)
+  players, posts = sim.execute(players, posts)
   t_similar, spearman = sim.results(posts)
 
 
