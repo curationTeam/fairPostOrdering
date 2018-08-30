954ba25aefb881f176d8805d7ab08d59607c2618
Orfeas Stefanos Thyfronitis Litos <o.thyfronitis@ed.ac.uk>
2018-08-30 12:38:53 +0100

    Plot in main, yield after every round

--- scripts/simulation/main.py
+++ scripts/simulation/main.py
@@ -1,5 +1,6 @@
 import random
 from simulation import Simulation
+import matplotlib.pyplot as plt
 
 sp = (1,) * (100+10)
 noProfiles = (100, 0, 10)
@@ -7,23 +8,33 @@ a = 1/200
 b = 0.01
 regen_time = 3 / (5*24*60*60)
 att_span = 5
+def plot(x, y, kind):
+    plt.plot(x, y)
+    plt.xlabel("Rounds")
+    plt.ylabel(kind)
+    plt.show()
+    name = "figures/" + str(x[-1]) + "_" + kind + ".png"
+    plt.savefig(name)
+
 
 def main():
     seed = random.randint(0, 1000)
     t_similar_list = []
     rounds = []
 
-    for i in range(1,1000,100):
-        # Simulation(rounds, noProfiles, a, b, regen_time, att_span)
-        sim = Simulation(sp, i, noProfiles, a, b, regen_time, att_span)
-        players, posts = sim.init_setup(seed)
-        players, posts = sim.execute(players, posts)
+    sim = Simulation(sp, noRound, noProfiles, a, b, regen_time, att_span)
+    players, posts = sim.init_setup(seed)
+    gen = sim.execute(players, posts)
+    for i in range(1, noRound):
+        players, posts = next(gen)
         t_similar, spearman, kendall_tau = sim.results(posts)
 
         rounds.append(i)
         t_similar_list.append(t_similar)
 
-    sim.plot(rounds, t_similar_list)
+    plot(rounds, t_similar_list, "t_similarity")
+    plot(rounds, spearman_list, "spearman")
+    plot(rounds, kendall_tau_list, "kendall_tau")
 
 if __name__== "__main__":
     main()
