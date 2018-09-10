a26fdd523d90a2be8d3967fa29a7491d79c03c2a
Orfeas Stefanos Thyfronitis Litos <o.thyfronitis@ed.ac.uk>
2018-09-10 17:03:30 +0100

    Write another way to trick Steem

--- simulation/main2.py
+++ simulation/main2.py
@@ -4,11 +4,10 @@ import matplotlib.pyplot as plt
 
 seed = random.randint(0, 1000)
 
-sim = Simulation((1,1), 3, (2,0,0), 0.02, 0.1, 0.3, 2, [[0.4, 1], [0.5, 0.8]])
+sim = Simulation((1,1,0), 4, (3,0,0), 0.5, 0.5, 0.5, 3, [[1, 0.8, 0], [1, 0.8, 0], [0.9, 1, 0]])
 players, posts = sim.init_setup(seed)
 gen = sim.execute(players, posts)
 
-players, posts = next(gen)
-print(posts)
-players, posts = next(gen)
-print(posts)
+for _i in range(0, 4):
+    players, posts = next(gen)
+    print(posts)
