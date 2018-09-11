678ed0b17964fb8f5542f7195e5092e5dc28f765
Orfeas Litos <o.thyfronitis@ed.ac.uk>
2018-09-11 01:35:07 +0100

    Generalize edgeCasesChecker

--- simulation/edgeCasesChecker.py
+++ simulation/edgeCasesChecker.py
@@ -4,10 +4,12 @@ import matplotlib.pyplot as plt
 
 seed = random.randint(0, 1000)
 
-sim = Simulation((1,1,0), 4, (3,0,0), 0.5, 0.5, 0.5, 3, [[1, 0.8, 0], [1, 0.8, 0], [0.9, 1, 0]])
+noPlayer = 341
+rounds = 523
+sim = Simulation((1,) * noPlayer, rounds, (noPlayer,0,0), 0.5, 0.5, 0.5, 3)
 players, posts = sim.init_setup(seed)
 gen = sim.execute(players, posts)
 
-for _i in range(0, 4):
+for _i in range(0, rounds):
     players, posts = next(gen)
     print(posts)
